4ac250901b96f0ad12e0cf45d4101910b8caecf5
Doniyorbek Anorboyev <doniyorbekanorboyev123@gmail.com>
2025-11-28 20:09:01 +0500

    Setup file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
-import 'pages/order/order_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'providers/cart_provider.dart';
+import 'screens/onboarding_screen.dart';
+
+import 'providers/favorites_provider.dart';
+import 'providers/address_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CoffeeShopApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeeShopApp extends StatelessWidget {
+  const CoffeeShopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => CartProvider()),
+        ChangeNotifierProvider(create: (ctx) => FavoritesProvider()),
+        ChangeNotifierProvider(create: (ctx) => AddressProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Coffee Shop App',
       debugShowCheckedModeBanner: false,
-      title: 'Order App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.brown,
+        primaryColor: const Color(0xFF6F4E37),
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
         useMaterial3: true,
       ),
-      home: const OrderPage(), 
+      home: const OnboardingScreen(),
+      ),
     );
   }
 }
