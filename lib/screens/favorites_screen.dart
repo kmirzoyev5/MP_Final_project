a174bf2a6a2e2bedbf5643ad722441748b0f4280
Doniyorbek Anorboyev <doniyorbekanorboyev123@gmail.com>
2025-11-28 20:34:40 +0500

    Implemented favorites screen UI and logic

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/product.dart';
 import '../providers/favorites_provider.dart';
 import '../widgets/product_card.dart';
-import 'product_detail_screen.dart';
+import '../pages/product_detail_screen.dart';
 
 class FavoritesScreen extends StatelessWidget {
   final List<Product> allProducts = [
@@ -81,4 +81,65 @@ class FavoritesScreen extends StatelessWidget {
       category: 'Cold Coffee',
     ),
   ];
+
+  FavoritesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final favoritesProvider = Provider.of<FavoritesProvider>(context);
+    final favoriteProducts = allProducts
+        .where((prod) => favoritesProvider.isFavorite(prod.id))
+        .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Favorites',
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: favoriteProducts.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite_border, size: 64, color: Colors.grey[400]),
+                  const SizedBox(height: 16),
+                  Text(
+                    'No favorites yet',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : GridView.builder(
+              padding: const EdgeInsets.all(24),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemCount: favoriteProducts.length,
+              itemBuilder: (context, index) {
+                return ProductCard(
+                  product: favoriteProducts[index],
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductDetailScreen(
+                          product: favoriteProducts[index],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+    );
+  }
 }
